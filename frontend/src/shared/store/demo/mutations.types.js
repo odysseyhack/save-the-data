ce38712366b8a0bdac592343a5e643e1f8ace506
jeystaats <jasper.staats@live.nl>
2019-04-14 00:44:57 +0200

    Add demo upload version

---
+++ frontend/src/shared/store/demo/mutations.types.js
@@ -0,0 +1,5 @@
+
+export const SET_DEMO_URL       = 'SET_DEMO_URL';
+export const SET_DEMO_SMOKE     = 'SET_DEMO_SMOKE';
+export const SET_DEMO_FIRE      = 'SET_DEMO_FIRE';
+export const SET_DEMO_QUADRANT  = 'SET_DEMO_QUADRANT';
