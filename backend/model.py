4b84469fba2d5aea0b6e0a53910575093920d828
Wouter Mostard <wouter.mostard@gmail.com>
2019-04-14 00:48:53 +0200

    Added number of high brightness pixels in roi

--- backend/model.py
+++ backend/model.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 CROP_SIZE = 100
 TARGET_SIZE = (224, 224)
 SMOKE_THRESHOLD = 0.3
+BRIGHTNESS_THRESHOLD = 200
+MINIMUM_NUMBER_BRIGHT = 5000    
 
 
 smoke_classifier = load_model('./models/ResNet50_scenario4.h5')
@@ -37,19 +39,24 @@ def get_number_of_patches():
                                 'x_index': x_index * CROP_SIZE, 
                                 'y_index': y_index * CROP_SIZE
                               }
+
                 sliding_example = retrieve_classification_example(test_image, coordinates)
                 
                 prediction = smoke_classifier.predict(sliding_example)[0]
 
                 if prediction[1] >= SMOKE_THRESHOLD:
                     smoke_pred = type_smoke_classifier.predict(sliding_example)[0][1]
-                    found_coordinates.append(coordinates)
+
                     densities.append(smoke_pred)
+                    found_coordinates.append((x_start, y_start, x_finish, y_finish, smoke_pred[0][1]))
+
+    image = np.asarray(Image.open(image_path))
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     return jsonify({'status': 'ok', 
                     'dark_smoke': 1 if np.mean(densities) > 0.8 else 0,
                     'coordinates': found_coordinates, 
-                    'fire': 1
+                    'high_brightness': retrieve_fire_roi(hsv, found_coordinates)
                     })
 
 
@@ -61,3 +68,26 @@ def retrieve_classification_example(image, indices):
     crop_array = np.asarray(crop)
 
     return np.expand_dims(crop_array, axis=0)
+
+
+def retrieve_fire_roi(hsv_image, num_found):
+    lowest_indices = np.unique(np.where(np.array(num_found)[:, 3].astype(int) == np.max([x[3] for x in num_found]))[0])
+    lowest_cells = np.array(num_found)[lowest_indices].astype(int)
+
+    left = np.min([x[0] for x in lowest_cells]) - 50
+    right = np.max([x[2] for x in lowest_cells]) + 50
+    top = np.max([x[3] for x in lowest_cells])
+    bottom = top + 100
+
+    fire_roi = np.asarray(hsv_image.crop((left, top, right, bottom)))
+
+    _, _, value = cv2.split(fire_roi)
+    number_above_threshold = np.unique(value.flatten() > BRIGHTNESS_THRESHOLD, return_counts=True)[1][1]
+
+    return number_above_threshold
+
+    # TODO: 
+    # if number_above_threshold > MINIMUM_NUMBER_BRIGHT:
+    #     return 1
+    # else:
+    #     return 0
