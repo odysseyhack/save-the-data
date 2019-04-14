cde3596db448e3271c2d245a46996f2feea7027d
Wouter Mostard <wouter.mostard@gmail.com>
2019-04-14 11:49:56 +0200

    Some comments

--- backend/model.py
+++ backend/model.py
@@ -25,6 +25,9 @@ app = Flask(__name__)
 
 @app.route('/retrieveImageFeatures')
 def get_number_of_patches():
+    """
+    Main method for running the classification algorithm
+    """
     filename = request.args.get('filename')
 
     test_image = Image.open('/srv/www/savethedata/project/api/public/images/{}'.format(filename)).resize((800, 600))
@@ -43,22 +46,31 @@ def get_number_of_patches():
 
                 sliding_example = retrieve_classification_example(test_image, cords)
                 
+                # Probability that an image patch is smoke
                 prediction = smoke_classifier.predict(sliding_example)[0]
 
                 if prediction[1] >= SMOKE_THRESHOLD:
+                    # Probability that the smoke is black
                     smoke_pred = type_smoke_classifier.predict(sliding_example)[0][1]
 
+                    # Add blackness to calculate the darkness of a cloud
                     densities.append(smoke_pred)
+
+                    # Store where we found the smoke 
                     found_coordinates.append([cords['x_index'], cords['y_index'], cords['x_finish'], cords['y_finish']])
 
     image = np.asarray(Image.open('/srv/www/savethedata/project/api/public/images/{}'.format(filename)).resize((800, 600)))
 
+    # If no smoke is found, we assume that there is no smoke
     if len(found_coordinates) == 0:
         high_brightness = 0
     else:
+        # Store the smoke predictions
+        store_smoke_predictions(Image.fromarray(image), densities, found_coordinates, filename)
+
+        # Load the Hue Saturation Value for retrieving brightness
         hsv = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2HSV))
 
-        store_smoke_predictions(Image.fromarray(image), densities, found_coordinates, filename)
         store_fire_prediction(image, np.asarray(found_coordinates), filename)
 
         high_brightness = retrieve_fire_roi(hsv, found_coordinates)
@@ -70,6 +82,10 @@ def get_number_of_patches():
 
 
 def retrieve_classification_example(image, cords):
+    """
+    Retrieves a crop from the target image and resizes it to the target size for the neural network.
+    Return a (1, TARGET_SIZE) numpy array
+    """
     crop = image.crop((cords['x_index'], cords['y_index'], cords['x_finish'], cords['y_finish'])).resize(TARGET_SIZE)
     crop_array = np.asarray(crop)
 
@@ -77,6 +93,11 @@ def retrieve_classification_example(image, cords):
 
 
 def retrieve_fire_roi(hsv_image, num_found):
+    """
+        To retrieve the possible fire location we look at the bottom of the fire. The location of the fire is located
+        by looking at the bottom of a smoke cluster. Subsequently we draw a box and retrieve the number of high brightness
+        pixels (Value in HSV). Lastly the image is stored. 
+    """
     lowest_indices = np.unique(np.where(np.array(num_found)[:, 3].astype(int) == np.max([x[3] for x in num_found]))[0])
     lowest_cells = np.array(num_found)[lowest_indices].astype(int)
 
@@ -96,6 +117,10 @@ def retrieve_fire_roi(hsv_image, num_found):
 
 
 def store_smoke_predictions(image, densities, coordinates, filename):
+    """
+      Draw rectangles around the image patches where we found smoke and store
+    """
+
     draw_image = image
     draw = ImageDraw.Draw(draw_image)
     for index, coord in enumerate(coordinates):
@@ -106,6 +131,10 @@ def store_smoke_predictions(image, densities, coordinates, filename):
 
 
 def store_fire_prediction(image, coordinates, filename):
+    """
+        Store the fire prediction
+    """
+
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     lowest_indices = np.unique(np.where(np.array(coordinates)[:, 3].astype(int) == np.max([x[3] for x in coordinates]))[0])
